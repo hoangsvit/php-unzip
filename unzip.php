30f785277e9ee0beb61a5ad7e836b619248ba7a2
hoangsvit <duatop.net@gmail.com>
2016-08-26 00:04:37 +0700

    Update unzip.php

--- unzip.php
+++ unzip.php
@@ -1,6 +1,6 @@
 <?php
-	$zip = new ZipArchive;
-if ($zip->open('htdocs.zip') === TRUE) { // File cần giải nén
+    $zip = new ZipArchive;
+    if ($zip->open('htdocs.zip') === TRUE) { // File cần giải nén
     $zip->extractTo('/home/u815960872/public_html/'); // Đường dẫn file
     $zip->close();
     echo 'Giải nén hoàn tất';
